81106b033f9977fec98fcd5445da75fc9edde436
rafal-jablonski <85371294+rafal-jablonski@users.noreply.github.com>
2023-10-06 17:30:03 +0200

    Feature/layout structure (#2)
    * Create core components, update modules and set up initial routing
    
    * Add all elements and styling to the first screen
    
    ---------
    
    Co-authored-by: rafal <rafal@rafals-MacBook-Pro.local>

--- src/app/modules/core/components/hello-world/hello-world.component.ts
+++ src/app/modules/core/components/hello-world/hello-world.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'portfolio-hello-world',
   templateUrl: './hello-world.component.html',
   styleUrls: ['./hello-world.component.scss']
 })
-export class HelloWorldComponent {
+export class HelloWorldComponent implements AfterViewInit {
+  @ViewChild('video') video!: ElementRef<HTMLVideoElement>;
 
+  ngAfterViewInit(): void {
+    this.changeVideoSpeed(0.75);
+  }
+
+  changeVideoSpeed(speed: number) {
+    const video = this.video.nativeElement as HTMLVideoElement;
+    video.playbackRate = speed;
+  }
 }
